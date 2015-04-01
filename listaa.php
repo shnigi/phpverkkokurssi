a1bdbaafced0ba7a99aedf512ba84b5014cfc69f
Niki Ahlskog <niki.ahlskog@gmail.com>
2015-04-01 21:42:47 +0300

    listaus

--- listaa.php
+++ listaa.php
@@ -1,30 +1,41 @@
 <?php require_once("inc/top.php"); ?>
 
-      <!-- Jumbotron -->
-      <div class="jumbotron">
-        <h1>Marketing stuff!</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
-        <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
-      </div>
-
-      <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-lg-4">
-          <h2>Safari bug warning!</h2>
-          <p class="text-danger">As of v8.0, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-lg-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-lg-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-        </div>
-      </div>
+     <div class="container top-margin">
+                  
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nimi</th>
+			 <th>Puhelinnumero</th>
+			  <th>Osoite</th>
+			   <th>Lisätietoja</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Anna</td>
+			 <td>123 123 1123</td>
+			 <td>Fisutie 1.A</td>
+			 <td>Tietojuttuja</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Debbie</td>
+			 <td>123 123 1123</td>
+			 <td>Fisutie 1.A</td>
+			 <td>Tietojuttuja</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>John</td>
+			 <td>123 123 1123</td>
+			 <td>Fisutie 1.A</td>
+			 <td>Tietojuttuja</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
 <?php require_once("inc/footer.php"); ?>
