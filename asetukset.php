070ba4a8f4a42a9d46478f9db971166bf95b1465
Niki Ahlskog <niki.ahlskog@gmail.com>
2015-04-26 17:42:14 +0300

    asetukset sivu

---
+++ asetukset.php
@@ -0,0 +1,11 @@
+<?php require_once("inc/top.php"); ?>
+
+      <!-- Jumbotron -->
+      <div class="jumbotron">
+        <h1>Asetukset</h1>
+   
+      </div>
+
+ 
+
+<?php require_once("inc/footer.php"); ?>
